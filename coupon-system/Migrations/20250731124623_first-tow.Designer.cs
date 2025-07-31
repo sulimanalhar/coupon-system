84aebcb1ffa5b86271d6a8ccc09362f33d7c1091
suliman <sulimansi582@gmail.com>
2025-07-31 04:24:16 -0900

    add migration

--- coupon-system/Migrations/20250731124623_first-tow.Designer.cs
+++ coupon-system/Migrations/20250731124623_first-tow.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using coupon_system.data;
 
@@ -11,9 +12,11 @@ using coupon_system.data;
 namespace coupon_system.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250731124623_first-tow")]
+    partial class firsttow
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
